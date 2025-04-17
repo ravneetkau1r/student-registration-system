0678dea53eb56b2e0cfa800ab5c4b9233002e0e1
ravneetkau1r <167840836+ravneetkau1r@users.noreply.github.com>
2025-04-17 16:11:10 -0600

    Update register.php

--- register.php
+++ register.php
@@ -8,11 +8,13 @@ try {
     $conn = new PDO("sqlsrv:server=$server;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Fetching the data from the form
     $firstName = htmlspecialchars($_POST['first_name']);
     $lastName = htmlspecialchars($_POST['last_name']);
     $email = htmlspecialchars($_POST['email']);
     $enrollmentDate = htmlspecialchars($_POST['enrollment_date']);
 
+    // Inserting into the database
     $stmt = $conn->prepare("INSERT INTO Students (FirstName, LastName, Email, EnrollmentDate) VALUES (?, ?, ?, ?)");
     $stmt->execute([$firstName, $lastName, $email, $enrollmentDate]);
 
